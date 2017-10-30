d50a69485678606c3a363dd8d5f96548972d7716
robincollins <racollins1@me.com>
2017-10-30 17:37:32 +0000

    update

--- lib/bank.rb
+++ lib/bank.rb
@@ -2,31 +2,31 @@ require_relative "statement"
 
 class Bank
 
-  attr_reader :statement
+  attr_reader :statement, :balance
 
   def initialize
     @balance = 0
     @statement = []
   end
 
-  def balance
-    @balance
-  end
+  # def balance # this is solely responsible for displaying the current balance
+  #   @balance
+  # end
 
-  def print_statement
-    puts "date   || cred/deb || balance"
+  def print_statement # this prints the statement as formatted output
+    puts "date || credit || debit || balance"
     @statement.each do |entry|
       puts entry.each { |p| p}
     end
   end
 
-  def deposit(amount)
+  def deposit(amount) # this handles deposits to the bank, adding that to the statement array in a certain way with a timestamp
     @balance += amount
     @statement << ["#{time_stamp} || #{amount} || || #{@balance}"]
     amount
   end
 
-  def withdraw(amount)
+  def withdraw(amount) # this handles withdrawals from the bank, adding that to the statement array in a certain way with a timestamp
     @balance -= amount
     @statement << ["#{time_stamp} || || #{amount} || #{@balance}"]
     amount
@@ -34,7 +34,7 @@ class Bank
 
   private
 
-    def time_stamp
+    def time_stamp # this is creating a time stamp, which is being called in deposit and withdraw methods
       Time.now.strftime("%d/%m/%Y")
     end
 
